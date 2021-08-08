5b59eda9670d50fca4f989b3fcc003dbc22e7b2b
ThatGust <73896944+ThatGust@users.noreply.github.com>
2021-08-07 22:09:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,22 +11,32 @@ app.config['MYSQL_DB'] = 'inventoryflask'
 
 mysql = MySQL(app)
 
-@app.route('/pythonlogin/', methods=['GET', 'POST'])
+@app.route('/inventoryflask/', methods=['GET', 'POST'])
 def login():
     msg = 'Error en el sistema'
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM admin WHERE username = %s AND password = %s', (username, password,))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return 'Logeo concedido'
+        else:
+            msg = 'Datos incorrectos'
     return render_template('index.html', msg='')
 
-@app.route('/pythonlogin/logout')
+@app.route('/inventoryflask/logout')
 def logout():
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
    return redirect(url_for('login'))
 
-@app.route('/pythonlogin/register', methods=['GET', 'POST'])
+@app.route('/inventoryflask/register', methods=['GET', 'POST'])
 def register():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
@@ -34,22 +44,22 @@ def register():
         password = request.form['password']
         email = request.form['email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM admin WHERE username = %s', (username,))
         account = cursor.fetchone()
         if account:
-            msg = 'Account already exists!'
+            msg = 'Existe ya una cuenta con ese nombre'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
+            msg = 'E-Mail invalido'
         elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
+            msg = 'El usuario solo debe contener caracteres validos'
         elif not username or not password or not email:
-            msg = 'Please fill out the form!'
+            msg = 'Porfavor rellene de forma correcta la forma'
         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            cursor.execute('INSERT INTO admin VALUES (NULL, %s, %s, %s)', (username, password, email,))
             mysql.connection.commit()
-            msg = 'You have successfully registered!'
+            msg = 'Esta registrado en la base de datos'
     elif request.method == 'POST':
-        msg = 'Please fill out the form!'
+        msg = 'Porfavor rellene la forma'
     return render_template('register.html', msg=msg)
 
 if __name__ == "__main__":
