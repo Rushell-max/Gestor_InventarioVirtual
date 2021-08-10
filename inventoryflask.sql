eb97710767723ae211cec492c22e4aa694562005
ThatGust <73896944+ThatGust@users.noreply.github.com>
2021-08-09 21:11:49 -0500

    Add files via upload

--- inventoryflask.sql
+++ inventoryflask.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-08-2021 a las 01:50:56
+-- Tiempo de generación: 10-08-2021 a las 04:11:00
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.2
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `id` int(100) NOT NULL,
-  `user` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `pass` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -38,8 +38,79 @@ CREATE TABLE `admin` (
 -- Volcado de datos para la tabla `admin`
 --
 
-INSERT INTO `admin` (`id`, `user`, `pass`, `email`) VALUES
-(1, 'admin', 'admin', 'admin@gmail.com');
+INSERT INTO `admin` (`id`, `username`, `password`, `email`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com'),
+(4, 'sssss', 'asfasf', 'afsafs@seses.es'),
+(5, 'asdasd', 'asdasdasdas', 'asdasd@sdsds.sdsa');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `elemento`
+--
+
+CREATE TABLE `elemento` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `marca` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `linea` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `subcategoria` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `unidades` int(100) NOT NULL,
+  `precio_original` int(100) NOT NULL,
+  `descuento` int(100) NOT NULL,
+  `proveedor` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `envio` int(100) NOT NULL,
+  `estado` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `fecha` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `tipo_de_compra` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `id` int(100) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marcas`
+--
+
+CREATE TABLE `marcas` (
+  `id` int(100) NOT NULL,
+  `lineas` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `subcategorias` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id` int(100) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `pagina` varchar(120) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id` int(100) NOT NULL,
+  `tipo` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Índices para tablas volcadas
@@ -52,6 +123,36 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `elemento`
+--
+ALTER TABLE `elemento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `marcas`
+--
+ALTER TABLE `marcas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -59,7 +160,37 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT de la tabla `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `elemento`
+--
+ALTER TABLE `elemento`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `marcas`
+--
+ALTER TABLE `marcas`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
